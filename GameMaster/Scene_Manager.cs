a9d08451bccc0a38e2ad6c3e4e7e68dcd5955061
Przemek <50340722+preemek@users.noreply.github.com>
2020-06-14 18:08:12 +0200

    Add files via upload

---
+++ GameMaster/Scene_Manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Scene_Manager
+{
+    // Start is called before the first frame update
+
+    public enum Scene
+    {
+        Menu,
+        First,
+        Planet
+    }
+    public static void Load(Scene scene)
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+   
+}
