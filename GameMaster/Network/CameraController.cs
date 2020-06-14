7ff31c3094a9870d4fada2f3d1962ff3868cfa9f
Przemek <50340722+preemek@users.noreply.github.com>
2020-06-14 18:07:58 +0200

    Add files via upload

---
+++ GameMaster/Network/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public PlayerManager player;
+    public float sensitivity = 100f;
+    public float clampAngle = 85f;
+
+    private float verticalRotation;
+    private float horizontalRotation;
+
+    private void Start()
+    {
+        verticalRotation = transform.localEulerAngles.x;
+        horizontalRotation = player.transform.eulerAngles.y;
+    }
+
+    private void Update()
+    {
+  
+        Debug.DrawRay(transform.position, transform.forward * 2, Color.red);
+    }
+
+
+}
